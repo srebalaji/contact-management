55e1f871dd3118ba9dba389e9456e6500930b681
Srebalaji Thirumalai <srebalaji@outlook.com>
2019-04-18 21:10:35 +0530

    Add Group service

--- services/index.js
+++ services/index.js
@@ -1,5 +1,7 @@
 const contactService = require('./contact-service')
+const contactGroupService = require('./contact-group-service')
 
 module.exports = {
   contactService,
+  contactGroupService,
 }
