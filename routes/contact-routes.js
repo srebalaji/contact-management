8398ef2f844afec878d53adb016cdeeba8677f0c
Srebalaji Thirumalai <srebalaji@outlook.com>
2019-04-18 20:55:17 +0530

    Complete contacts
    1. Complete CURD for contacts

--- routes/contact-routes.js
+++ routes/contact-routes.js
@@ -6,7 +6,16 @@ const {
 
 const router = express.Router()
 
+// List all contacts
+router.get('/', responseHandler((req, res) => contactService.list(req, res)))
+
 // create new contact
 router.post('/', responseHandler((req, res) => contactService.create(req, res)))
+
+// Update contact
+router.put('/:id', responseHandler((req, res) => contactService.update(req, res)))
+
+// Delete contact
+router.delete('/:id', responseHandler((req, res) => contactService.deleteContact(req, res)))
 
 module.exports = router
--- services/contact-service.js
+++ services/contact-service.js
@@ -1,5 +1,11 @@
 const Contact = require('../models/contact')
 
+// List all contacts
+const list = async () => {
+  const contacts = Contact.find({}).sort({ createdAt: -1 })
+  return contacts
+}
+
 // Create contact
 const create = async (req) => {
   const contact = new Contact(req.body)
@@ -13,6 +19,43 @@ const create = async (req) => {
   }
 }
 
+// Update contact
+const update = async (req) => {
+  if (req.params.id) {
+    try {
+      return Contact.findOneAndUpdate(
+        { _id: req.params.id },
+        req.body,
+        { runValidators: true, context: 'query', new: true },
+      )
+    } catch (e) {
+      if (e.name === 'ValidationError' && e.message) {
+        throw new Error(e.message)
+      }
+      throw new Error('Error in updating contact.')
+    }
+  }
+  throw new Error('No contact id specified.')
+}
+
+// Delete a contact
+const deleteContact = async (req) => {
+  if (req.params.id) {
+    try {
+      return Contact.findOneAndRemove({ _id: req.params.id })
+    } catch (e) {
+      if (e.name === 'ValidationError' && e.message) {
+        throw new Error(e.message)
+      }
+      throw new Error('Error in updating contact.')
+    }
+  }
+  throw new Error('No contact id specified')
+}
+
 module.exports = {
   create,
+  list,
+  update,
+  deleteContact,
 }
