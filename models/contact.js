8f2f70ca66ed1e6e6b81ecc62aff9c8f016c7a06
Srebalaji Thirumalai <srebalaji@outlook.com>
2019-04-19 18:56:18 +0530

    Add contact model validations

--- models/contact.js
+++ models/contact.js
@@ -1,3 +1,5 @@
+/* eslint-disable func-names */
+/* eslint-disable prefer-arrow-callback */
 
 const mongoose = require('mongoose')
 const uniqueValidator = require('mongoose-unique-validator')
@@ -11,18 +13,27 @@ const contact = new Schema(
   {
     name: {
       type: String,
-      lowercase: true,
       required: [true, 'Contact name is mandatory'],
       unique: true,
+      uniqueCaseInsensitive: true,
     },
-    details: [
-      person,
-    ],
+    details: {
+      type: [person],
+      required: [true, 'Contact details are mandatory'],
+    },
   },
   { timestamps: true },
 )
 
-
+// Validate if contact details are present
+contact.pre('validate', function (next) {
+  if (this.details.length === 0) {
+    next(new Error('Contact details are mandatory'))
+    return false
+  }
+  next()
+  return true
+})
 contact.plugin(uniqueValidator, { message: 'Contact name is already taken' })
 
 module.exports = mongoose.model('contact', contact)
